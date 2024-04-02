697ce576c2d06649b3b2d10ad6c0c6ff898fff5f
pg745 <60575976+nbgsmk@users.noreply.github.com>
2024-04-02 16:47:10 +0200

    Refactored

--- NavDrawer/src/main/java/cc/kostic/navdraw/App.java
+++ NavDrawer/src/main/java/cc/kostic/navdraw/App.java
@@ -14,18 +14,18 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import cc.kostic.navdraw.databinding.ActivityMainBinding;
+import cc.kostic.navdraw.databinding.AppBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class App extends AppCompatActivity {
 
-	private AppBarConfiguration mAppBarConfiguration;
-	private ActivityMainBinding binding;
+	private AppBarConfiguration appBarConfiguration;
+	private AppBinding binding;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		binding = ActivityMainBinding.inflate(getLayoutInflater());
+		binding = AppBinding.inflate(getLayoutInflater());
 		setContentView(binding.getRoot());
 
 		setSupportActionBar(binding.appBarMain.toolbar);
@@ -39,27 +39,25 @@ public class MainActivity extends AppCompatActivity {
 		});
 		DrawerLayout drawer = binding.drawerLayout;
 		NavigationView navigationView = binding.navView;
-		// Passing each menu ID as a set of Ids because each
-		// menu should be considered as top level destinations.
-		mAppBarConfiguration = new AppBarConfiguration.Builder(
-				R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-				.setOpenableLayout(drawer)
-				.build();
-		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-		NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+		AppBarConfiguration.Builder builder = new AppBarConfiguration.Builder(
+				R.id.nav_home,
+				R.id.nav_gallery,
+				R.id.nav_slideshow);
+		appBarConfiguration = builder.setOpenableLayout(drawer).build();
+		NavController navController = Navigation.findNavController(this, R.id.nav_host__content_main);
+		NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 		NavigationUI.setupWithNavController(navigationView, navController);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.menu_toolbar, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onSupportNavigateUp() {
-		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-		return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+		NavController navController = Navigation.findNavController(this, R.id.nav_host__content_main);
+		return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
 	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,5 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "NavDraw"
-include(":app")
+include(":NavDrawer")
  
